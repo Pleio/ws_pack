0eb2e383145aec47a80cd74c53c6a46de0cbc1b0
Bart Jeukendrup <bart@jeukendrup.nl>
2016-12-05 20:08:11 +0100

    only show public, logged_in and site results

--- lib/webservices/search.php
+++ lib/webservices/search.php
@@ -32,10 +32,17 @@
 			$ia = elgg_set_ignore_access(true);
 		}
 
-		$results = ESInterface::get()->search($query, "all", null, null, (int) $limit, (int) $offset);
+		$access_ids = array(ACCESS_PUBLIC, ACCESS_LOGGED_IN);
+		$site = elgg_get_site_entity();
+		if ($subsite_acl = $site->getPrivateSetting("subsite_acl")) {
+			$access_ids[] = $subsite_acl;
+		};
+
+		$results = ESInterface::get()->search($query, "all", null, null, (int) $limit, (int) $offset, "", "", 0, array(), $access_ids);
 
 		$json = array();
 		$json["total"] = $results["count"];
+
 		if ($results["count"] > 0) {
 			$json["results"] = ws_pack_export_entities($results["hits"]);
 		} else {
